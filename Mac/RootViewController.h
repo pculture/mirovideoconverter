88d80bbfd4c70a4cf51e0f6b9fc46eac678d8243
C Worth <cworth@C-Worths-MacBook-Pro-2.local>
2010-02-25 15:06:19 -0400

    Add output window and console process creation

--- Mac/RootViewController.h
+++ Mac/RootViewController.h
@@ -12,19 +12,34 @@
 
 @class ClickableText;
 
-typedef enum { initialView, withFileView, convertingView, finishedView } ViewMode;
-
+typedef enum { ViewRoot, ViewConverting } Views;
+typedef enum { ViewModeInitial, ViewModeWithFile, ViewModeConverting, ViewModeFinished } ViewMode;
+typedef enum { FFMPEGStatusConverting, FFMPEGStatusDone, FFMPEGStatusError } FFMPEGStatus;
 @interface RootViewController : NSObject <DropBoxViewDelegate>{
+  NSView *rootView;
+  NSTextField *convertAVideo;
   NSTextField *dragAVideo;
   ClickableText *chooseAFile1;
   NSTextField *toSelectADifferent;
   ClickableText *chooseAFile2;
+  NSTextField *finishedConverting;
+  NSTextField *showFile;      
   NSPopUpButton *devicePicker;
   NSButton *convertButton;
   NSTextField *filename;
   DropBoxView *dropBox;
   NSWindow *window;
+  NSView *convertingView;
+  NSTextField *convertingFilename;	 
+  NSTextField *percentDone;		 
+  NSProgressIndicator *progressIndicator;
+  NSWindow *fFMPEGOutputWindow;
+  NSTextView *fFMPEGOutputTextView;
 }
+@property(nonatomic,retain) IBOutlet NSTextField *convertAVideo;
+@property(nonatomic,retain) IBOutlet NSTextField *finishedConverting;
+@property(nonatomic,retain) IBOutlet NSTextField *showFile;      
+@property(nonatomic,retain) IBOutlet NSView *rootView;
 @property(nonatomic,retain) IBOutlet NSTextField *dragAVideo;
 @property(nonatomic,retain) IBOutlet NSTextField *chooseAFile1;
 @property(nonatomic,retain) IBOutlet NSTextField *toSelectADifferent;
@@ -34,11 +49,27 @@ typedef enum { initialView, withFileView, convertingView, finishedView } ViewMod
 @property(nonatomic,retain) IBOutlet NSTextField *filename;
 @property(nonatomic,retain) IBOutlet DropBoxView *dropBox;
 @property(nonatomic,retain) IBOutlet NSWindow *window;
+@property(nonatomic,retain) IBOutlet NSView *convertingView;
+@property(nonatomic,retain) IBOutlet NSTextField *convertingFilename;	    
+@property(nonatomic,retain) IBOutlet NSTextField *percentDone;		    
+@property(nonatomic,retain) IBOutlet NSProgressIndicator *progressIndicator;
+@property(nonatomic,retain) IBOutlet NSWindow *fFMPEGOutputWindow;
+@property(nonatomic,retain) IBOutlet NSTextView *fFMPEGOutputTextView;
 
+-(void) loadConvertingView;
 -(void) setViewMode:(ViewMode)viewMode;
+-(void) setAlphaValuesForViewMode:(ViewMode)viewMode;
 -(NSString*) formatFilename:(NSString *)inFile;
 -(IBAction) chooseAFile:(id)sender;
 -(IBAction) selectADevice:(id)sender;
 -(IBAction) convertButtonClick:(id)sender;
 -(void) maybeEnableConvertButton;
+-(void) showView:(int)whichView;
+-(IBAction) cancelButtonClick:(id)sender;
+-(IBAction) fFMPEGButtonClick:(id)sender;
+-(void) convertingDone;
+-(void) setDonePercentage:(NSNumber *)percent;
+-(void) doFFMPEGConversion;
+-(char *) fFMPEGCommandLine;
+-(FFMPEGStatus) parseFFMPEGOutput:(NSTextStorage *)storage fromPosition:(int)position;
 @end
--- Mac/RootViewController.m
+++ Mac/RootViewController.m
@@ -13,22 +13,65 @@
 #import "DropBoxView.h"
 
 @implementation RootViewController
-
-@synthesize dragAVideo,chooseAFile1,toSelectADifferent,chooseAFile2,devicePicker,convertButton,filename,dropBox,window;
+@synthesize rootView,convertAVideo,dragAVideo,chooseAFile1,toSelectADifferent,chooseAFile2;
+@synthesize devicePicker,convertButton,filename,dropBox,window;
+@synthesize finishedConverting, showFile;      
+@synthesize convertingView, convertingFilename, percentDone, progressIndicator;
+@synthesize fFMPEGOutputWindow,fFMPEGOutputTextView;
 
 -(void) awakeFromNib {
-  [devicePicker removeAllItems];
-  [devicePicker addItemWithTitle:@"Pick a Device or Video Format"];
-  [devicePicker addItemWithTitle:@"PSP"];
-  [devicePicker addItemWithTitle:@"OGG"];
-  [devicePicker addItemWithTitle:@"Theora"];
-  [devicePicker selectItemAtIndex:0];
-  [self setViewMode:initialView];
+  static BOOL firstTime = YES;
+  if(firstTime){
+    [self setViewMode:ViewModeInitial];
+    [devicePicker removeAllItems];
+    [devicePicker addItemWithTitle:@"Pick a Device or Video Format"];
+    [devicePicker addItemWithTitle:@"G1"];
+    [devicePicker addItemWithTitle:@"PSP"];
+    [devicePicker addItemWithTitle:@"Theora"];
+    [convertButton setEnabled:NO];
+    [dropBox registerForDraggedTypes: [NSArray arrayWithObjects:NSFilenamesPboardType, nil]];
+    firstTime = NO;
+  }
+}
+-(void) loadConvertingView {
+  [NSBundle loadNibNamed:@"Converting" owner:self];
+  [progressIndicator setMinValue:0];
+  [progressIndicator setMaxValue:100];
+  [progressIndicator setIndeterminate:NO];
+  [NSBundle loadNibNamed:@"FFMPEGOutputWindow" owner:self];
 }
 -(void) setViewMode:(ViewMode)viewMode{
-
   switch(viewMode) {
-  case initialView:
+  case ViewModeInitial:
+    [self showView:ViewRoot];
+    [devicePicker selectItemAtIndex:0];
+    [self maybeEnableConvertButton];
+    break;
+  case ViewModeWithFile:
+    [self showView:ViewRoot];
+    [self maybeEnableConvertButton];
+    break;
+  case ViewModeConverting:
+    [self showView:ViewConverting];
+    [convertingFilename setStringValue:[filename stringValue]];
+    [self setDonePercentage:[NSNumber numberWithDouble:0]];
+    [progressIndicator startAnimation:self];
+    [NSThread detachNewThreadSelector:@selector(doFFMPEGConversion) toTarget:self withObject:nil];
+    break;
+  case ViewModeFinished:
+    [self showView:ViewRoot];
+    [devicePicker selectItemAtIndex:0];
+    [self maybeEnableConvertButton];
+    break;
+  default:
+    break;
+  }
+  [self setAlphaValuesForViewMode:viewMode];
+}
+-(void) setAlphaValuesForViewMode:(ViewMode)viewMode{
+  switch(viewMode) {
+  case ViewModeInitial:
+    convertAVideo.alphaValue =      1;
     dragAVideo.alphaValue =         1;
     chooseAFile1.alphaValue =       1;
     toSelectADifferent.alphaValue = 0;
@@ -36,11 +79,11 @@
     devicePicker.alphaValue =       1;
     convertButton.alphaValue =      1;
     filename.alphaValue =           0;
-    [convertButton setTitle:@"Convert"];
-    [convertButton setEnabled:NO];
-    [dropBox registerForDraggedTypes: [NSArray arrayWithObjects:NSFilenamesPboardType, nil]];
+    finishedConverting.alphaValue = 0;
+    showFile.alphaValue =           0;
     break;
-  case withFileView:
+  case ViewModeWithFile:
+    convertAVideo.alphaValue =      1;
     dragAVideo.alphaValue =         0;
     chooseAFile1.alphaValue =       0;
     toSelectADifferent.alphaValue = 1;
@@ -48,34 +91,25 @@
     devicePicker.alphaValue =       1;
     convertButton.alphaValue =      1;
     filename.alphaValue =           1;
-    [convertButton setTitle:@"Convert"];
-    [dropBox registerForDraggedTypes: [NSArray arrayWithObjects:NSFilenamesPboardType, nil]];
+    finishedConverting.alphaValue = 0;
+    showFile.alphaValue =           0;
     break;
-  case convertingView:
-    dragAVideo.alphaValue =         0;
-    chooseAFile1.alphaValue =       0;
+  case ViewModeConverting:
+    break;
+  case ViewModeFinished:
+    convertAVideo.alphaValue =      0;
+    dragAVideo.alphaValue =         1;
+    chooseAFile1.alphaValue =       1;
     toSelectADifferent.alphaValue = 0;
     chooseAFile2.alphaValue =       0;
     devicePicker.alphaValue =       1;
     convertButton.alphaValue =      1;
     filename.alphaValue =           0;
-    [convertButton setTitle:@"Convert"];
-    [dropBox registerForDraggedTypes: [NSArray arrayWithObjects:NSFilenamesPboardType, nil]];
-    break;
-  case finishedView:
-    dragAVideo.alphaValue =         1;
-    chooseAFile1.alphaValue =       1;
-    toSelectADifferent.alphaValue = 1;
-    chooseAFile2.alphaValue =       0;
-    devicePicker.alphaValue =       1;
-    convertButton.alphaValue =      1;
-    filename.alphaValue =           0;
-    [convertButton setTitle:@"Convert"];
-    [dropBox registerForDraggedTypes: [NSArray arrayWithObjects:NSFilenamesPboardType, nil]];
+    finishedConverting.alphaValue = 1;
+    showFile.alphaValue =           1;
     break;
   default:
     break;
-    
   }
 }
 - (NSString *)formatFilename:(NSString *)inFile {
@@ -93,7 +127,7 @@
 }
 - (void)dropBoxView:(DropBoxView *)dropBoxView fileDropped:(NSString *)aFilename {
   [filename setStringValue:[self formatFilename:aFilename]];
-  [self setViewMode:withFileView];
+  [self setViewMode:ViewModeWithFile];
   [self maybeEnableConvertButton];
 }
 -(IBAction) chooseAFile:(id)sender {
@@ -110,7 +144,7 @@
   if(returnCode == NSOKButton) {
     [filename setStringValue:[self formatFilename:[[sheet filenames] objectAtIndex:0]]];
     [sheet close];
-    [self setViewMode:withFileView];
+    [self setViewMode:ViewModeWithFile];
   }
   [self maybeEnableConvertButton];
 }
@@ -123,7 +157,77 @@
   else
     [convertButton setEnabled:NO];
 }
+-(void) showView:(int)whichView {
+  NSView *theView;
+  switch(whichView) {
+  case ViewRoot:
+    theView = rootView;
+    break;
+  case ViewConverting:
+    if(!convertingView)
+      [self loadConvertingView];
+    theView = convertingView;
+    break;
+  default:
+    break;
+  }
+  if([window contentView] != theView){
+    [[window contentView] removeFromSuperview];
+    [window setContentView:theView];
+  }
+}
+
 -(IBAction) convertButtonClick:(id)sender {
-  convertButton.alphaValue=0;
+  [self setViewMode:ViewModeConverting];
+}
+-(IBAction) cancelButtonClick:(id)sender {
+ int iResponse = 
+        NSRunAlertPanel(@"Cancel Conversion",@"Are you sure you want cancel the conversion?",
+                        @"No", @"Yes", /*third button*/nil/*,args for a printf-style msg go here*/);
+  switch(iResponse) {
+    case NSAlertDefaultReturn:
+      break;
+    case NSAlertAlternateReturn:
+      [NSThread 
+      [self setViewMode:ViewModeWithFile];
+      break;
+  }
+}
+-(void) convertingDone {
+  [progressIndicator stopAnimation:self];
+  [finishedConverting setStringValue:[NSString stringWithFormat:@"Finished converting %@", [filename stringValue]]];
+  [self setViewMode:ViewModeFinished];
+}
+-(void) setDonePercentage:(NSNumber *)percent {
+  [progressIndicator setDoubleValue:[percent doubleValue]];
+  [percentDone setStringValue:[NSString stringWithFormat:@"%i%% done",[percent intValue]]];
+}
+-(char *) fFMPEGCommandLine {
+  char *commandLine = malloc(1024);
+  sprintf(commandLine,"tail -f ~/watchFile.txt");
+  return commandLine;
+}
+-(FFMPEGStatus) parseFFMPEGOutput:(NSTextStorage *)storage fromPosition:(int)position {
+  return FFMPEGStatusConverting;
+}
+-(void) doFFMPEGConversion {
+  NSAutoreleasePool *pool =[[NSAutoreleasePool alloc] init];
+  NSTextStorage *storage = [[[fFMPEGOutputTextView textContainer] textView] textStorage];
+  char *commandLine = [self fFMPEGCommandLine];
+  FILE *fp = popen(commandLine,"r");
+  int bufsize = 1024; char *buf = malloc(bufsize);
+  int fFMPEGStatus = FFMPEGStatusConverting, textPosition = 0;
+  while(fFMPEGStatus == FFMPEGStatusConverting){
+    if(fgets(buf,bufsize,fp)){
+      [storage replaceCharactersInRange:NSMakeRange([storage length], 0)
+	       withString:[NSString stringWithCString:buf]]; 
+      fFMPEGStatus = [self parseFFMPEGOutput:storage fromPosition:textPosition];
+      textPosition = [storage length];
+    }
+    usleep(100000);
+  }
+  fclose(fp); free(commandLine);
+  [self performSelectorOnMainThread:@selector(convertingDone) withObject:nil waitUntilDone:NO];
+  [pool release];
 }
 @end
