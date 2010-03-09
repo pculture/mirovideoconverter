3052d0ec5e79f0c168910fd460c0e46ca109d783
Adam Duston <aduston@gmail.com>
2010-03-09 16:48:07 -0500

    Automated build and deployment process

---
+++ MSWindows/build.py
@@ -0,0 +1,61 @@
+import sys, os, re
+import uuid
+
+def update_msi_version():
+    print "Updating vdproj file"
+    product_code = str(uuid.uuid4()).upper()
+    package_code = str(uuid.uuid4()).upper()
+    lines = []
+    with open('WindowsSetup\\WindowsSetup.vdproj', 'r') as f:
+        for line in f.readlines():
+            old_line = line
+            if re.search(r"ProductCode\" = \"8:\{", line) is not None:
+                line = re.sub("8:\{[A-Z0-9\-]+\}", "8:{%s}" % product_code, line)
+            if re.search(r"PackageCode\" = \"8:\{", line) is not None:
+                line = re.sub("8:\{[A-Z0-9\-]+\}", "8:{%s}" % package_code, line)
+            product_version_match = re.search(
+                r"ProductVersion\" = \"8:(\d+\.\d+\.\d+)", line)
+            if product_version_match is not None:
+                old_product_version = product_version_match.groups(1)[0]
+                new_product_version = \
+                    '.'.join(`int(old_product_version.replace('.','')) + 1`)
+                line = re.sub(old_product_version, new_product_version, line)
+            if line != old_line:
+                print "Replacing %s\nwith      %s\n" % (old_line, line)
+            lines.append(line)
+    with open('WindowsSetup\\WindowsSetup.vdproj', 'w') as f:
+        for line in lines:
+            f.write("%s" % line)
+
+def build():
+    print "Building"
+    for line in os.popen("devenv /build Release FFMPEGWrapper.sln /project WindowsSetup").readlines():
+        print line
+
+def find_assembly_version_no():
+    with open('Windows\\Properties\\AssemblyInfo.cs', 'r') as f:
+        for line in f.readlines():
+            assembly_version_match = re.search(
+                r"^\[assembly: AssemblyVersion\(\"([\d\.]+)\"\)", 
+                line)
+            if assembly_version_match is not None:
+                return assembly_version_match.groups(1)[0]
+
+def make_version_file():
+    print "Making version file"
+    version_no = find_assembly_version_no()
+    print "Detected assembly version %s" % version_no
+    with open('c:\\temp\\miroconverterversion.xml', 'w') as f:
+        f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
+        f.write("<version>%s</version>" % version_no)
+
+def upload_to_server():
+    
+        
+def main(argv):
+    update_msi_version()
+    build()
+    make_version_file()
+    upload_to_server()
+
+main(sys.argv[1:])
