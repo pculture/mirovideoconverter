4e88e052f08dea2cbcf3f18322c88719db198d47
Adam Duston <aduston@gmail.com>
2010-03-22 14:45:43 -0400

    Added minor option

--- MSWindows/signexe.py
+++ MSWindows/signexe.py
@@ -14,14 +14,14 @@ pfx_file = os.path.join(os.path.dirname(__file__),
 
 opts, args = getopt.getopt(sys.argv[1:], "m", ["msi"])
 if len(opts) > 0 and opts[0][0] in ("-m", "--msi"):
-    description_option = "-d Miro Video Converter"
+    description_option = "/d \"Miro Video Converter\""
     target_file = os.path.join(os.path.dirname(__file__), 
         "WindowsSetup\\Release\\MiroConverterSetup.msi")
 else:
     description_option = ""
     target_file = os.path.join(os.path.dirname(__file__), 
         "Windows\\bin\\Release\\MiroConverter.exe")
-command = ("signtool sign /f {0} /p {1} {2}").format(
-    pfx_file, password, target_file)
+command = ("signtool sign /f {0} /p {1} {2} {3}").format(
+    pfx_file, password, description_option, target_file)
 for line in os.popen(command).readlines():
     print line
