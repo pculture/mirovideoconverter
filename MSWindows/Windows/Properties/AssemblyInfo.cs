b4da23e7996968f839b6ddf4637fbc96bec71f05
Adam Duston <aduston@gmail.com>
2010-03-27 17:58:07 -0400

    Inserted GPL license text into each source file

--- MSWindows/Windows/Properties/AssemblyInfo.cs
+++ MSWindows/Windows/Properties/AssemblyInfo.cs
@@ -1,3 +1,23 @@
+//  MiroVideoConverter -- a super simple way to convert almost any video to MP4, 
+//  Ogg Theora, or a specific phone or iPod.
+//
+//  Copyright 2010 Participatory Culture Foundation
+//
+//  This file is part of MiroVideoConverter.
+//
+//  MiroVideoConverter is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  MiroVideoConverter is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MiroVideoConverter.  If not, see http://www.gnu.org/licenses/.
+
 using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
@@ -51,5 +71,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.010324.00")]
-[assembly: AssemblyFileVersion("1.0.010324.00")]
+[assembly: AssemblyVersion("1.0.010327.16")]
+[assembly: AssemblyFileVersion("1.0.010327.16")]
