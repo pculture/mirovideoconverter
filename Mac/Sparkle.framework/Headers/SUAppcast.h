bc915a08590abbb35f9d7cd2570c845f3fcd71ff
C Worth <cworth@C-Worths-MacBook-Pro.local>
2010-05-06 15:44:34 -0400

    bug 13084: added licensing and readme files.  Removed test videos.  Removed duplicate licensing from headeres of Sparkle framework.

--- Mac/Sparkle.framework/Headers/SUAppcast.h
+++ Mac/Sparkle.framework/Headers/SUAppcast.h
@@ -1,23 +1,3 @@
-//  MiroVideoConverter -- a super simple way to convert almost any video to MP4, 
-//  Ogg Theora, or a specific phone or iPod.
-//
-//  Copyright 2010 Participatory Culture Foundation
-//
-//  This file is part of MiroVideoConverter.
-//
-//  MiroVideoConverter is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  MiroVideoConverter is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with MiroVideoConverter.  If not, see http://www.gnu.org/licenses/.
-
 //
 //  SUAppcast.h
 //  Sparkle
